4feca0683068fdd6e5f930dec87dfef11ffd9304
Noah Jarvis <noah.jarvis@connectwise.com>
2020-03-30 10:54:00 -0400

    remove prints

--- mcs/class/System.ServiceModel/System.ServiceModel.Description/ContractDescriptionGenerator.cs
+++ mcs/class/System.ServiceModel/System.ServiceModel.Description/ContractDescriptionGenerator.cs
@@ -278,7 +278,6 @@ namespace System.ServiceModel.Description
 			for (int i = 0; i < contractMethods.Length; ++i)
 			{
 				MethodInfo mi = contractMethods [i];
-				Console.WriteLine(mi.Name + ": " + mi.ReturnType + ": sub: " + mi.ReturnType.IsSubclassOf(typeof(Task))  + ": assign: " + mi.ReturnType.IsAssignableFrom(typeof(Task)));
 				if (mi.ReturnType.IsAssignableFrom(typeof(Task)) || mi.ReturnType.IsSubclassOf(typeof(Task)))
 					continue;
 				OperationContractAttribute oca = GetOperationContractAttribute (mi);
